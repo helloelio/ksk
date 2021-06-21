2c4a60bd5d9e425522436e9284ef5d9d8f8321de
helloelio <70343671+helloelio@users.noreply.github.com>
2021-06-21 12:38:52 +0300

    some fixes

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 //              кнопка *добавить*
 const openCardButton = document.querySelector('.add-button');
-
 //              модальное окно(окно добавления)
 const createMenu = document.querySelector('.modal');
-
 //              кнопка закрытия модального окна
-const closeModalButton = document.querySelector('.close');
-
+const closeCreateModalButton = document.querySelector('.close');
+//
+const changeModal = document.querySelector('.change-modal');
+//
+const closeEditModalButton = document.querySelector('.change-close');
 //              блок с кнопками карты
 const cardMenuBtns = document.querySelectorAll('.menu-btn');
-
 //              окно редактирования для карточки
 const editMenus = document.querySelectorAll('.edit-modal');
-
 //              блок с карточками
 let cards = document.querySelector('.cards');
-
 //              строка ввода данных в модальном окне добавления
 let inputCardNumber = document.getElementById('card-number');
-
 //              кнопка 'добавить' в модальном окне добавления
 let createCardButton = document.querySelector('.create-card');
-
 //              открытие модального окна добавления "товара"
 let checkboxDrag = document.getElementById('checkbox-drag');
 console.log(checkboxDrag);
@@ -45,7 +41,7 @@ let openCardMenu = () => {
 };
 
 //              закрытие модального окна добавления нажатием на "крестик x"
-let closeCardMenuByButton = () => {
+let closeCreateCardMenuByButton = () => {
     createMenu.style.display = 'none';
     inputCardNumber.value = '';
     inputCardNumber.removeAttribute('autofocus');
@@ -58,6 +54,17 @@ let closeCardMenuByKey = (event) => {
         inputCardNumber.removeAttribute('autofocus');
     }
 };
+//
+let closeChangeModalMenuByButton = () => {
+    changeModal.classList.remove('change-modal-flex');
+};
+let closeChangeModalMenuByKey = (event) => {
+    if (event.key == 'Escape') {
+        changeModal.classList.remove('change-modal-flex');
+    }
+};
+
+// check box check
 checkboxDrag.checked = false;
 console.log(checkboxDrag.checked);
 checkboxDrag.addEventListener('change', () => {
@@ -87,7 +94,7 @@ let createCard = (
                 <div class="card__name">${inputCardNumber}</div>
                 <div class="card__buttons">
                     <div class="edit-modal edit-modal-${id}">
-                    <button class="edit-btn"><img src="./icons/edit.png" style="margin-right: 10px;"><span>редактировать</span></button>
+                    <button class="edit-btn edit-btn-${id}"><img src="./icons/edit.png" style="margin-right: 10px;"><span>редактировать</span></button>
                     <button class="delete-btn delete-btn-${id}"><img src="./icons/close.png" style="margin-right: 10px;"><span>удалить</span></button>
                     </div>
                     <button class="drag-btn drag-btn-${id}"><img src="./icons/move.png"></button>
@@ -106,16 +113,17 @@ let createCard = (
 
     // check the statement
     if (!isInit) {
-        const cardsStorage = localStorage.getItem('cards')
-            ? JSON.parse(localStorage.getItem('cards'))
+        const cardsStorage = localStorage.getItem(`cards-${id}`)
+            ? JSON.parse(localStorage.getItem(`cards-${id}`))
             : [];
+        console.log(cardsStorage);
         cardsStorage.push({
             id: id,
             formatedDate: formatedDate,
             selectOptionType: selectOptionType,
             inputCardNumber: inputCardNumber,
         });
-        localStorage.setItem('cards', JSON.stringify(cardsStorage));
+        localStorage.setItem(`cards-${id}`, JSON.stringify(cardsStorage));
     }
 
     // open menu to drag/delete
@@ -125,17 +133,26 @@ let createCard = (
             .classList.toggle('edit-modal-flex');
     });
 
-    // delete button from(menu)
+    // delete card by button from(menu)
     document
         .querySelector(`.delete-btn-${id}`)
         .addEventListener('click', (event) => {
             console.log(document.querySelector(`.card-${id}`));
             document.querySelector(`.card-${id}`).remove();
-            let arr = JSON.parse(localStorage.getItem('cards')) ?? {};
-            delete arr[id];
-            localStorage.setItem('cards', JSON.stringify(arr));
-        });
+            localStorage.removeItem(`cards-${id}`);
 
+            // let arr = JSON.parse(localStorage.getItem(`cards-${id}`)) ?? {};
+            // delete arr[id];
+            // localStorage.setItem(`cards-${id}`, JSON.stringify(arr));
+        });
+    // edit card by button from(menu)
+    document.querySelector(`.edit-btn-${id}`).addEventListener('click', () => {
+        document
+            .querySelector(`.edit-modal-${id}`)
+            .classList.remove('edit-modal-flex');
+        document.querySelector('#change-card-number').value = inputCardNumber;
+        changeModal.classList.add('change-modal-flex');
+    });
     // drag card by button
     document
         .querySelector(`.drag-btn-${id}`)
@@ -145,13 +162,6 @@ let createCard = (
         });
     return;
 };
-
-// open modal (нажатие на кнопку + добавить)
-openCardButton.addEventListener('click', openCardMenu);
-// close by button
-closeModalButton.addEventListener('click', closeCardMenuByButton);
-// closs by ESC
-window.addEventListener('keydown', closeCardMenuByKey);
 
 // create card by click on (createCardButton)
 createCardButton.addEventListener('click', () => {
@@ -191,22 +201,65 @@ createMenu.addEventListener('keydown', (event) => {
     }
 });
 
+// edit card fucntion
+// document.querySelector('.change-card').addEventListener('click', () => {
+//     console.log('lol');
+//     const inputCardNumber = document.querySelector('#card-number').value;
+//     const selectOptionType = document.querySelector('#create-select').value;
+//     const currentDate = new Date();
+//     const formatedDate =
+//         currentDate.getDate() +
+//         '-' +
+//         (currentDate.getMonth() + 1) +
+//         '-' +
+//         currentDate.getFullYear();
+//     createCard(selectOptionType, inputCardNumber, formatedDate, count);
+//     count++;
+//     localStorage.setItem('count', count);
+//     createMenu.style.display = 'none';
+//     document.querySelector('#card-number').value = '';
+// });
+
+// open modal (нажатие на кнопку + добавить)
+openCardButton.addEventListener('click', openCardMenu);
+// close modal
+closeCreateModalButton.addEventListener('click', closeCreateCardMenuByButton);
+window.addEventListener('keydown', closeCardMenuByKey);
+
+// close change modal
+closeEditModalButton.addEventListener('click', closeChangeModalMenuByButton);
+window.addEventListener('keydown', closeChangeModalMenuByKey);
+
 function initCards() {
-    const cards = JSON.parse(localStorage.getItem('cards'));
+    let cards = [];
+    for (let i = 0; i <= 10; i++) {
+        cards.push(JSON.parse(localStorage.getItem(`cards-${i}`)));
+    }
     for (let card of cards) {
         if (card == null) {
             continue;
         }
         createCard(
-            card.selectOptionType,
-            card.inputCardNumber,
-            card.formatedDate,
-            card.id,
+            card[0].selectOptionType,
+            card[0].inputCardNumber,
+            card[0].formatedDate,
+            card[0].id,
             true
         );
     }
 }
 initCards();
+var _lsTotal = 0,
+    _xLen,
+    _x;
+for (_x in localStorage) {
+    if (!localStorage.hasOwnProperty(_x)) {
+        continue;
+    }
+    _xLen = (localStorage[_x].length + _x.length) * 2;
+    _lsTotal += _xLen;
+}
+console.log('Total = ' + (_lsTotal / 1024).toFixed(2) + ' KB');
 //
 // TODO: fix cards
 // TODO: localstorage
